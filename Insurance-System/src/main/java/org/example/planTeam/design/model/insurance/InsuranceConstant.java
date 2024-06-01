56a7765f7d3c16cde3f3279cbe592c554e1b4624
0702Yoon <gkstkddbs99@naver.com>
2024-06-01 13:37:09 +0900

    feat : Insurance 상수 추가

---
+++ Insurance-System/src/main/java/org/example/planTeam/design/model/insurance/InsuranceConstant.java
@@ -0,0 +1,11 @@
+package org.example.planTeam.design.model.insurance;
+
+public class InsuranceConstant {
+
+    public static final String INSURANCE_NAME = "insuranceName";
+    public static final String RESPONSIBLE_PERSON = "responsiblePerson";
+    public static final String RESTRICTION_REGULATION = "restrictionRegulation";
+    public static final String SUBSCRIBER_RIGHTS_AND_OBLIGATION = "subscriberRightObligation";
+    public static final String INSURANCE = "insurance";
+
+}
