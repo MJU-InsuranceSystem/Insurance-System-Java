b0caed69e49e54342dd720d2e5f5ec46a29d7b78
TaetaetaE01 <gogo589@naver.com>
2024-06-17 01:11:55 +0900

    Refactor : 고객이 사건정보 입력 및 면부책 판단

--- Insurance-System/src/main/java/org/example/common/config/MainSystemConfig.java
+++ Insurance-System/src/main/java/org/example/common/config/MainSystemConfig.java
@@ -9,37 +9,38 @@ import org.example.common.view.SystemView;
 import org.example.insurance.InsuranceApplyListImpl;
 import org.example.insurance.InsuranceChargeCustomerApplyList;
 import org.example.insurance.InsuranceChargeCustomerApplyListImpl;
+import org.example.rewardSupportTeam.model.AccidentListImpl;
 import org.example.user.CustomerProcessManager;
 import org.example.user.CustomerView;
 
 public class MainSystemConfig {
 
-  public FrontController frontController() {
-    return new FrontController(systemView(), authController(), customerSystem(), workerSystem());
-  }
+    public FrontController frontController() {
+        return new FrontController(systemView(), authController(), customerSystem(), workerSystem());
+    }
 
-  private static SystemView systemView() {
-    return new InsuranceSystemView();
-  }
+    private static SystemView systemView() {
+        return new InsuranceSystemView();
+    }
 
-  private AuthController authController() {
-    return new AuthController(systemView());
-  }
+    private AuthController authController() {
+        return new AuthController(systemView());
+    }
 
-  private CustomerSystem customerSystem() {
-    return new CustomerSystem(systemView(), customerView(), customerProcessManager());
-  }
+    private CustomerSystem customerSystem() {
+        return new CustomerSystem(systemView(), customerView(), customerProcessManager());
+    }
 
-  private CustomerView customerView() {
-    return new CustomerView();
-  }
+    private CustomerView customerView() {
+        return new CustomerView();
+    }
 
-  private CustomerProcessManager customerProcessManager() {
-    return new CustomerProcessManager(new InsuranceApplyListImpl(), new InsuranceChargeCustomerApplyListImpl());
-  }
+    private CustomerProcessManager customerProcessManager() {
+        return new CustomerProcessManager(new InsuranceApplyListImpl(), new InsuranceChargeCustomerApplyListImpl(), new AccidentListImpl());
+    }
 
-  private WorkerSystem workerSystem() {
-    return new WorkerSystem(systemView());
-  }
+    private WorkerSystem workerSystem() {
+        return new WorkerSystem(systemView());
+    }
 
 }
--- Insurance-System/src/main/java/org/example/rewardSupportTeam/RewardSupportTeam.java
+++ Insurance-System/src/main/java/org/example/rewardSupportTeam/RewardSupportTeam.java
@@ -48,20 +48,13 @@ public class RewardSupportTeam extends Team {
     @Override
     public ResponseDto process(RequestDto request) {
         ResponseDto responseDto = new ResponseDto();
+
         if (request.get(JUDGE_ANSWER).equals("Y") || request.get(JUDGE_ANSWER).equals("y")) {
             // 면부책을 판단한다
-            // list에 정보가 다 있고 계약에서 어떤 보험에 가입했는지에 따라 면부책 판단을 내려주면 될 듯
-//            Customer applyUser = insuranceChargeCustomerApplyList.findFirst();
-//
-//            if (applyUser == null) {
-//                responseDto.add(Status.getKey(), Status.FAIL.getStatus());
-//                return responseDto;
-//            }
-//            List<Contract> tempList = applyUser.getContractList().getContracts();
             Accident accident = accidentList.read(0);
             if (accident == null) {
                 judgmentResult = false;
-                responseDto.add(Status.getKey(), Status.FAIL.getStatus());
+                responseDto.add(Status.getKey(), Status.EMPTY.getStatus());
                 return responseDto;
             }
             judgmentResult = true;
@@ -77,29 +70,29 @@ public class RewardSupportTeam extends Team {
     @Override
     public ResponseDto register(RequestDto request) {
         ResponseDto responseDto = new ResponseDto();
-
-        if (!insuranceChargeCustomerApplyList.isEmpty()) {
-            Accident accident = new Accident();
-            accident.setContent(request.get(ACCIDENT_CONTENT));
-            accident.setCustomerName(request.get(ACCIDENT_NAME));
-
-            ClaimInsurance claimInsurance = new ClaimInsurance();
-            claimInsurance.setAccount(request.get(CLAIMINSURANCE_ACCOUNT));
-            claimInsurance.setAddress(request.get(CLAIMINSURANCE_ADDRESS));
-            claimInsurance.setPhoneNumber(request.get(CLAIMINSURANCE_PHONENUMBER));
-            claimInsurance.setResidentNumber(request.get(CLAIMINSURANCE_RESIDENTNUMBER));
-            claimInsurance.setSupportingFile(request.get(CLAIMINSURANCE_SUPPORTINGFILE));
-
-            accident.setClaimInsurance(claimInsurance);
-
-            if (!accidentList.add(accident)) {
-                responseDto.add(Status.getKey(), Status.FAIL.getStatus());
-                return responseDto;
-            }
-            responseDto.add(Status.getKey(), Status.SUCCESS.getStatus());
-            return responseDto;
-        }
-        responseDto.add(Status.getKey(), Status.EMPTY.getStatus());
+//
+//        if (!insuranceChargeCustomerApplyList.isEmpty()) {
+//            Accident accident = new Accident();
+//            accident.setContent(request.get(ACCIDENT_CONTENT));
+//            accident.setCustomerName(request.get(ACCIDENT_NAME));
+//
+//            ClaimInsurance claimInsurance = new ClaimInsurance();
+//            claimInsurance.setAccount(request.get(CLAIMINSURANCE_ACCOUNT));
+//            claimInsurance.setAddress(request.get(CLAIMINSURANCE_ADDRESS));
+//            claimInsurance.setPhoneNumber(request.get(CLAIMINSURANCE_PHONENUMBER));
+//            claimInsurance.setResidentNumber(request.get(CLAIMINSURANCE_RESIDENTNUMBER));
+//            claimInsurance.setSupportingFile(request.get(CLAIMINSURANCE_SUPPORTINGFILE));
+//
+//            accident.setClaimInsurance(claimInsurance);
+//
+//            if (!accidentList.add(accident)) {
+//                responseDto.add(Status.getKey(), Status.FAIL.getStatus());
+//                return responseDto;
+//            }
+//            responseDto.add(Status.getKey(), Status.SUCCESS.getStatus());
+//            return responseDto;
+//        }
+//        responseDto.add(Status.getKey(), Status.EMPTY.getStatus());
         return responseDto;
     }
 
